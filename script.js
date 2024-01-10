5565ceef13942863874bb6be5079bc52ea5e44cc
dabi2 <88178441+dabi2@users.noreply.github.com>
2024-01-10 10:59:33 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
+const inputBox = document.getElementById("inpt");
+const taskList = document.getElementById("task_list");
 
+function addTask(){
+    if(inputBox.value ===''){
+        alert("You must add Something")
+    }
+    else{
+        let li = document.createElement("li"); //it is creating an HTML element 
+        //for example when we add a text the jscript function is creating a --li element
+        li.innerHTML = inputBox.value; // whatever text wwe have in the input box
+        //it will display in th --li element
+        taskList.appendChild(li);
+        let span = document.createElement("span");
+        span.innerHTML = "\u00d7";
+        li.appendChild(span);
+
+    }
+    inputBox.value = "";
+}
